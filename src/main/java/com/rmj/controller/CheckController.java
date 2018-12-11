e8417b2fe23deb51094d6a435592ad178b083abb
Alishahian <salishah@bbyfs.ca>
2018-12-10 18:54:35 -0800

    added new controller

---
+++ src/main/java/com/rmj/controller/CheckController.java
@@ -0,0 +1,35 @@
+package com.rmj.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+
+@Controller
+@RequestMapping("/api")
+public class CheckController {
+ 
+
+
+    @RequestMapping(value = "/sendmsg/{sid}/{token}/{number}", method = RequestMethod.GET) 
+    public @ResponseBody String getStatus(@PathVariable String sid, @PathVariable String token, @PathVariable String number) {
+    	try {
+    		Twilio.init(sid, token);
+    		Message message = Message.creator(
+    		    new PhoneNumber("+12225559999"),
+    		    new PhoneNumber(number),
+    		    "Sample Twilio SMS using Java")
+    		.create();
+     		   return "Heelo Dada";
+ 		  } catch (Exception e) {
+ 		   	   return "Error" ;
+ 		  }
+    }
+    
+
+}
